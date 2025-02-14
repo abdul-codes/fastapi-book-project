b47017fe2946bcf6bccd5c793c8a55cadcf04fa3
abdul-codes <abdulsalamkinsanya2@gmail.com>
2025-02-14 11:03:39 +0000

    Keep local changes to books.py

--- api/routes/books.py
+++ api/routes/books.py
@@ -47,6 +47,13 @@ async def create_book(book: Book):
 async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
+@router.get("/{book_id}", response_model=Book)
+async def get_book_by_id(book_id: int):
+    book = db.books.get(book_id)
+    if book:
+        return JSONResponse(status_code=status.HTTP_200_OK, content=book.model_dump())
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book not found")
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def update_book(book_id: int, book: Book) -> Book:
